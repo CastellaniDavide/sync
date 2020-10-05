83e4b136c51e54a36bb3657ef700f3196c4e802f
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-10-05 10:14:34 +0200

    Created bin/template.py

---
+++ bin/sync.py
@@ -0,0 +1,19 @@
+"""sync
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-10-5"
+
+class sync:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(sync.sync())
+	
+	def sync():
+		"""sync first funtion
+		"""
+		return "sync" #for the test (see test_sync.py)
+		
+if __name__ == "__main__":
+	sync()
