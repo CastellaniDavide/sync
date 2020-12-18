7bd9b09484b1cb3c45b39c0d8fd9a8bc14ed7ad7
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-12-18 22:08:15 +0100

    Now the python code is better; added GitHub Action

--- bin/sync.py
+++ bin/sync.py
@@ -6,45 +6,54 @@ import pymysql
 from datetime import datetime
 
 __author__ = "help@castellanidavide.it"
-__version__ = "01.01 2020-10-08"
+__version__ = "01.01 2020-12-18"
 
 class sync:
-	def __init__ (self, debug=False, db=True, vs=False):
+	def __init__ (self, folder=None, debug=False, vs=False):
 		"""Where it all begins
 		"""
-		base_dir = "." if vs else ".." # the project "root" in Visual studio it is different
+		#Setup basic variabiles
+		self.start_time = datetime.now()
+		self.debug = debug
+		self.vs = vs
 
-		log = open(os.path.join(base_dir, "log", "trace.log"), "a+")
-		local_files = open(os.path.join(base_dir, "flussi", "file_to_upload_and_where.csv"), "r").read()
-		sync.log(log, "Opened all files")
+		if folder == None:
+			self.folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flussi")
+		else:
+			self.folder = folder
+
+		self.log = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "log", f"{self.start_time.strftime('%Y%m%d')}sync.log"), "a+")
+		self.local_files = open(os.path.join(self.folder, "file_to_upload_and_where.csv"), "r").read()
 		
-		start_time = datetime.now()
-		sync.log(log, f"Start time (python): {start_time}")
-		sync.log(log, "Running: sync.py")
+		self.print(f"Start")
+		self.print(f"Opened all files")
+		self.print("Running: sync.py")	
 
 		# Sync offline
-		sync.copy(local_files, base_dir, log, debug)
+		self.copy()
 		
 		# Sync online if possible
 		try:
-			sync.sync(local_files, base_dir, log, debug)
+			self.online_sync_all()
 		except:
-			sync.print_and_log(log, "Internet/ DB(s) not avariable", debug)
+			self.print("Internet/ DB(s) not avariable")
 		
 		# End
-		sync.log(log, f"End time: {datetime.now()}\nTotal time (python): {datetime.now() - start_time}")
-		if debug : sync.log(log, "")
-		log.close()
+		self.print(f"End time: {datetime.now()}")
+		self.print(f"Total time (python): {datetime.now() - self.start_time}")
+		self.log.close()
 
-	def copy(local_files, base_dir, log, debug):
+	def copy(self):
 		""" Copy all files in flussi folder
 		"""
-		
-		for line in sync.csv2array(local_files)[1:]:
-			file_path = line[0] + line[1]
-			open(os.path.join(base_dir, "flussi", "cloned", f"""{sync.PC_name()}_{line[1]}"""), "w+").write(open(file_path).read())
+		try:
+			for line in sync.csv2array(local_files)[1:]:
+				file_path = line[0] + line[1]
+				open(os.path.join(self.folder, "flussi", "cloned", f"""{sync.PC_name()}_{line[1]}"""), "w+").write(open(file_path).read())
 
-		sync.print_and_log(log, "All files cloned", debug)
+			self.print("All files copied offline")
+		except:
+			self.print("Nothing to copy offline")
 
 	def make_csv_standard(data):
 		"""Convert my text in csv standard to prevent errors
@@ -56,32 +65,11 @@ class sync:
 		data.encode("ASCII")
 		return data
 
-	def log(file, item):
-		"""Writes a line in the log.log file
-		"""
-		file.write(f"{item}\n")
-
-	def print_and_log(file, item, debug):
+	def print(self, item):
 		"""Writes on the screen and in the log file
 		"""
-		if debug : print(item)
-		sync.log(file, item)
-
-	def init_csv(flie, intestation, log):
-		"""Init the csv files
-		"""
-		flie.write(f"{intestation}\n")
-		sync.log(log, "csv now initialized")
-
-	def print_db(db_file, intestation, log, debug, tablename_main="sync"):
-		"""Checks the database content
-		"""
-		# Main content
-		if debug : print("\nDatabase main content:")
-		if debug : print(intestation.replace("\\\\", "\\"))
-
-		for row in db_file.execute(f"SELECT * FROM {tablename_main}").fetchall():
-			if debug : print(str(row)[1:-1].replace("'", "").replace("\\\\", "\\").replace("\\\\", "\\"))
+		if self.debug : print(item)
+		self.log.write(f""""{self.start_time.timestamp()}","{item}","{datetime.now().timestamp()}"\n""")
 
 	def PC_name():
 		""" Return the PC user-name in standard mode
@@ -126,41 +114,50 @@ class sync:
 
 		return text
 
-	def sync(local_files, base_dir, log, debug):
+	def online_sync_all(self):
 		""" If possible update all
 		"""
-		for i, file in enumerate(sync.csv2array(local_files)[1:]):
-			sync.print_and_log(log, f" - {i}° File", debug)
+		for i, file in enumerate(sync.csv2array(self.local_files)[1:]):
+			self.print(f" - {i}° File")
 			
+			# Get configuration
 			host = file[2]
 			user = file[4]
 			password = file[5]
 			database = file[3]
 			tablename = file[1].replace(".csv", "")
+
+			# Connenct to the DB
 			connection = pymysql.connect(host, user, password, database)
-			sync.print_and_log(log, f"   - Connected {i}° database", debug)
+			self.print(f"   - Connected {i}° database")
 
-			with connection.cursor() as cursor:
-				file_to_sync = sync.csv2array(open(os.path.join(base_dir, "flussi", "cloned", f"""{sync.PC_name()}_{file[1]}"""), "r").read())
+			self.sync_online_single(file, connection)
+
+	def sync_online_single(self, file, connection):
+		""" Sync a single file
+		"""
+		with connection.cursor() as cursor:
+			file_to_sync = sync.csv2array(open(os.path.join(base_dir, "flussi", "cloned", f"""{sync.PC_name()}_{file[1]}"""), "r").read())
 				
-				# If not exist create database
-				variabiles = sync.array2csv([[f"""{a.replace(' ', '_')}""" for a in file_to_sync[0]],]).replace('""', '"').replace('"', '').replace('\n', '').replace('\\', '').replace('/', '')
-				variabiles_with_type = sync.array2csv([[f"""{a.replace(' ', '_')} varchar(255)""" for a in file_to_sync[0]],]).replace('""', '"').replace('"', '').replace('\n', '').replace('\\', '').replace('/', '')
+			# If not exist create database
+			variabiles = sync.array2csv([[f"""{a.replace(' ', '_')}""" for a in file_to_sync[0]],]).replace('""', '"').replace('"', '').replace('\n', '').replace('\\', '').replace('/', '')
+			
+			cursor.execute(f"""CREATE TABLE IF NOT EXISTS {tablename} (ID int AUTO_INCREMENT, {variabiles} varchar(255), PRIMARY KEY (ID));""")
+			self.print(f"   - Connected {i}° table")
 
-				cursor.execute(f"""CREATE TABLE IF NOT EXISTS {tablename} (ID int AUTO_INCREMENT, {variabiles_with_type}, PRIMARY KEY (ID));""")
-				sync.print_and_log(log, f"   - Connected {i}° table", debug)
+			# Add all new items
+			for j, items in enumerate(file_to_sync[1:]):
+				items = sync.array2csv([items,])[:-1:]
 
-				# Add all new items
-				for j, items in enumerate(file_to_sync[1:]):
-					items = sync.array2csv([items,])[:-1:]
+				cursor.execute(f"SELECT * FROM {tablename} WHERE ({variabiles}) = ({items});")
+				if len(cursor.fetchall()) == 0: # If not exist add it
+					cursor.execute(f"INSERT INTO {tablename} ({variabiles}) VALUES ({items});")
+					self.print(f"   - Values added ({items})")
 
-					cursor.execute(f"SELECT * FROM {tablename} WHERE ({variabiles}) = ({items});")
-					if len(cursor.fetchall()) == 0:
-						cursor.execute(f"INSERT INTO {tablename} ({variabiles}) VALUES ({items});")
-						sync.print_and_log(log, f"   - Values added ({items})", debug)
+		# Push changes
+		connection.commit()
+		connection.close()
 
-			connection.commit()
-			connection.close()
 
 if __name__ == "__main__":
 	# debug flag
